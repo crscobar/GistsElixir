6e92365f27a5229f8ee35eac9b682e386ce568c8
eschriscobar <chris.escobar12@gmail.com>
2024-03-25 23:35:45 -0500

    Clearing fields with JS event handlers (LiveView Ep. 12)

--- elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
+++ elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
@@ -25,6 +25,7 @@ defmodule ElixirGistWeb.CreateGistLive do
   def handle_event("create", %{"gist" => params}, socket) do
     case Gists.create_gist(socket.assigns.current_user, params) do
       {:ok, _gist} ->
+        socket = push_event(socket, "clear-textareas", %{})
         empty_changeset = Gists.change_gist(%Gist{})
         {:noreply, assign(socket, :form, to_form(empty_changeset))}
       {:error, %Ecto.Changeset{} = error_changeset} ->
