c9da616c550258aebecf62929edfb6727bb23928
eschriscobar <chris.escobar12@gmail.com>
2024-03-26 21:28:04 -0500

    More Live components!! (LiveView Ep. 20)

--- elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
+++ elixir_gist/lib/elixir_gist_web/live/create_gist_live.ex
@@ -1,7 +1,27 @@
 defmodule ElixirGistWeb.CreateGistLive do
   use ElixirGistWeb, :live_view
 
+  alias ElixirGist.{Gists.Gist, Gists}
+  alias ElixirGistWeb.GistFormComponent
+
   def mount(_params, _session, socket) do
    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="gist-gradient flex items-center justify-center">
+      <h1 class="font-brand font-bold text-3xl text-white">
+        Instantly share Elixir code, notes, and snippets.
+      </h1>
+    </div>
+
+    <.live_component
+      module={GistFormComponent}
+      id={:new}
+      current_user={@current_user}
+      form={to_form(Gists.change_gist(%Gist{}))}
+    />
+    """
   end
 end
